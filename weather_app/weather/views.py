a9670a6600145562ac2ef4bf474f179fb1cb07a2
Алеська <1912977@mail.ru>
2022-10-20 15:01:22 +0300

    weather_city

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
+    appid = '2c619c63181e61385cbf2c53c504b559'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    city = 'Moskva'
+    res = requests.get(url.format(city))
+
+    print(res.text)
     return render(request, 'weather/index.html')
 
 
