ac9d68b125263d549d3595c2f473c3f74c9fbce4
Алеська <1912977@mail.ru>
2022-10-21 18:12:32 +0300

    weather_temp

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -5,12 +5,16 @@ import requests
 def index(request):
     appid = '2c619c63181e61385cbf2c53c504b559'
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
-    city = 'Moskva'
-    res = requests.get(url.format(city))
+    city = 'Москва'
+    res = requests.get(url.format(city)).json()
 
-    print(res.text)
-    return render(request, 'weather/index.html')
+    city_info = {'city': city, 'temp': res['main']['temp'], 'icon': res['weather'][0]['icon'],
+                 'temp_max': res['main']['temp_max'], 'temp_min': res['main']['temp_min'],
+                 'humidity': res['main']['humidity']}
+    context = {'info': city_info}
+    return render(request, 'weather/index.html', context)
 
 
 def detail(request):
+
     return render(request, 'weather/detail.html')
