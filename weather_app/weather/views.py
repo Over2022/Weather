201eece65c570ae3e838c1a8c7ef8db4e9eda5c2
Алеська <1912977@mail.ru>
2022-10-31 17:12:05 +0300

    weather_css

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
+from .models import City
+from .forms import CityForm
 
 
 def index(request):
     appid = '2c619c63181e61385cbf2c53c504b559'
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
-    city = 'Москва'
-    res = requests.get(url.format(city)).json()
+    if request.method == 'GET':
+        cities = City.objects.all().delete()
 
-    city_info = {'city': city, 'temp': res['main']['temp'], 'icon': res['weather'][0]['icon'],
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+    form = CityForm()
+    cities = City.objects.all()
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {'city': city, 'temp': res['main']['temp'], 'icon': res['weather'][0]['icon'],
                  'temp_max': res['main']['temp_max'], 'temp_min': res['main']['temp_min'],
                  'humidity': res['main']['humidity']}
-    context = {'info': city_info}
+        all_cities.append(city_info)
+        print(all_cities)
+
+    context = {'all_cities': all_cities, 'form': form}
     return render(request, 'weather/index.html', context)
 
 
